69c6976ea52661a68b4849bd968d97f9f0bb502b
Peter gabriel MORGAN PARDO <pmorgan-@e3r9p3.42.fr>
2019-04-05 14:36:30 +0200

    script

---
+++ script.sh
@@ -0,0 +1,30 @@
+echo "EX00"
+php -f ex00/test.php
+echo
+echo "EX01"
+php -f ex01/test1.php 
+echo
+php -f ex01/test2.php
+echo
+echo "EX02"
+php -f ex02/test.php
+echo
+echo "EX03"
+php -f ex03/test1.php
+echo
+php -f ex03/test2.php
+echo
+echo "EX04"
+php -f ex04/test.php
+echo
+echo "EX05"
+php -f ex05/test1.php
+echo
+php -f ex05/test2.php
+echo
+echo "EX06"
+php -f ex06/test1.php
+echo
+php -f ex06/test2.php
+echo
+
